be9bf54c165bd404a39a575a623c3b00840d7764
Monali <monalidoshi9@gmail.com>
2021-07-27 08:20:44 +0530

    Format comments

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,15 +2,15 @@ const jwt = require("jsonwebtoken");
 const apiResponse = require("../helpers/apiResponse");
 
 module.exports = function (req, res, next) {
-	//Get token from header
+	// Get token from header
 	const token = req.header("x-auth-token");
 
-	//Check if no token
+	// Check if no token
 	if (!token) {
 		return apiResponse.unauthorizedResponse(res, "No token");
 	}
 
-	//Verify token
+	// Verify token
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		req.user = decoded.user;
