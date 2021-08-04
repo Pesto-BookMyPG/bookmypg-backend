64c2972cf0bbfe6ca78b13ad0b0037b9af8eca25
Monali <monalidoshi9@gmail.com>
2021-08-04 17:40:43 +0530

    Add new endpoint and route protection code

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,15 @@ var userRouter = require("./user");
 var amenityRouter = require("./amenity");
 var propertyRouter = require("./property");
 var locationRouter = require("./location");
+var reviewRouter = require("./review");
+var complaintRouter = require("./complaint");
 var app = express();
 
 app.use("/user/", userRouter);
 app.use("/amenity/", amenityRouter);
 app.use("/property/", propertyRouter);
 app.use("/location/", locationRouter);
+app.use("/review/", reviewRouter);
+app.use("/complaint/", complaintRouter);
 
 module.exports = app;
